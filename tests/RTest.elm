57c15966aa6485eb62ed91c912627f5142fefc5a
Kevin Lee <klee@MSI.localdomain>
2020-05-01 23:09:08 -0700

    Didnt format test i guess?

--- tests/RTest.elm
+++ tests/RTest.elm
@@ -1,8 +1,8 @@
-module Example exposing (..)
+module RTest exposing (..)
 
-import Rationalizer exposing (..)
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
+import Rationalizer exposing (..)
 import Test exposing (..)
 
 
@@ -11,8 +11,11 @@ suite =
     describe "Testing valid ingredient inputs"
         [ test "just numbers" <|
             \_ ->
-                let test1 = "10"
-                in Expect.false "Expect this to fail" (isValidIngred test1)
+                let
+                    test1 =
+                        "10"
+                in
+                Expect.false "Expect this to fail" (isValidIngred test1)
         , test "ingredient with no unit" <|
             \_ ->
                 "2 chickens"
